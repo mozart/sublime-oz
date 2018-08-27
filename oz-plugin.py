6f52d19c9e5aed4c96d835070ebeef71d7f50605
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2018-08-27 14:47:28 +0200

    Check if compilation fails

--- oz-plugin.py
+++ oz-plugin.py
@@ -7,9 +7,10 @@ def compile_mozart(file):
     process = Popen(['ozc', '-c', file], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     process.wait()
+    return_code = process.poll()
     outs = stdout.decode('utf-8')
     errs = stderr.decode('utf-8')
-    return [outs, errs]
+    return [outs, errs, return_code]
 
 def execute_mozart(file):
     process = Popen(['ozengine', file], stdout=PIPE, stderr=PIPE)
@@ -22,19 +23,21 @@ def execute_mozart(file):
 class OzRunCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         file = self.view.file_name()
-        [out_comp, err_comp] = compile_mozart(file)
-        [out_exec, err_exec] = execute_mozart(file+'f')
+        co = compile_mozart(file)
+        [out_comp, err_comp, code_comp] = compile_mozart(file)
         window = sublime.active_window()
         panel = window.find_output_panel('oz_panel')
         if(panel == None):
             panel = window.create_output_panel('oz_panel')
+        window.run_command('show_panel', {'panel':'output.oz_panel'})
         panel.set_read_only(False)
         panel.run_command("append", {"characters": "Compilation output :\n"})
         panel.run_command("append", {"characters": out_comp+"\n"+err_comp+"\n"})
-        panel.run_command("append", {"characters": "Execution output:\n"})
-        panel.run_command("append", {"characters": out_exec+"\n"+err_exec+"\n"})
+        if(code_comp == 0):
+            [out_exec, err_exec] = execute_mozart(file+'f')
+            panel.run_command("append", {"characters": "Execution output:\n"})
+            panel.run_command("append", {"characters": out_exec+"\n"+err_exec+"\n"})
         panel.set_read_only(True)
-        window.run_command('show_panel', {'panel':'output.oz_panel'})
     def is_visible(self):
         print(self.view.window().active_panel())
         return True
