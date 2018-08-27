d613c3122ddebbde9033e15bc1511c022c160b62
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2018-08-27 13:58:17 +0200

    Show compilation/execution on new panel

--- oz-plugin.py
+++ oz-plugin.py
@@ -24,9 +24,17 @@ class OzRunCommand(sublime_plugin.TextCommand):
         file = self.view.file_name()
         [out_comp, err_comp] = compile_mozart(file)
         [out_exec, err_exec] = execute_mozart(file+'f')
-        print("Compilation Output :")
-        print(out_comp)
-        print(err_comp)
-        print("\nExecution Output : ")
-        print(out_exec)
-        print(err_exec)
+        window = sublime.active_window()
+        panel = window.find_output_panel('oz_panel')
+        if(panel == None):
+            panel = window.create_output_panel('oz_panel')
+        panel.set_read_only(False)
+        panel.run_command("append", {"characters": "Compilation output :\n"})
+        panel.run_command("append", {"characters": out_comp+"\n"+err_comp+"\n"})
+        panel.run_command("append", {"characters": "Execution output:\n"})
+        panel.run_command("append", {"characters": out_exec+"\n"+err_exec+"\n"})
+        panel.set_read_only(True)
+        window.run_command('show_panel', {'panel':'output.oz_panel'})
+    def is_visible(self):
+        print(self.view.window().active_panel())
+        return True
