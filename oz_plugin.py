995dbace5a550c88052e913a47df70e0d269b739
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2018-09-03 12:50:36 +0200

    Start ozengine at feed buffer if necessary

--- oz_plugin.py
+++ oz_plugin.py
@@ -10,21 +10,26 @@ sp = None
 def kill_oz(sock):
     sock.send("{Application.exit 0}\n\004\n\n}")
 
+def start_oz():
+    global sp
+    #Panel to display compilation and emulator output
+    window = sublime.active_window()
+    panel = window.find_output_panel('oz_panel')
+    if(panel == None):
+        panel = window.create_output_panel('oz_panel')
+    window.run_command('show_panel', {'panel':'output.oz_panel'})
+    sp = OzThread(panel)
+    sp.start()
+
 class OzRunCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        global sp
-        #Panel to display compilation and emulator output
-        window = sublime.active_window()
-        panel = window.find_output_panel('oz_panel')
-        if(panel == None):
-            panel = window.create_output_panel('oz_panel')
-        window.run_command('show_panel', {'panel':'output.oz_panel'})
-        sp = OzThread(panel)
-        sp.start()
+        start_oz()
 
 class OzFeedBufferCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global sp
+        if(sp == None):
+            start_oz()
         msg = self.view.substr(sublime.Region(0, self.view.size()))
         msg = msg + "\n\004\n"
         sp.send(msg)
