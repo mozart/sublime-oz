5dd92b88b9345b7d625524448745d54816015b16
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2018-09-05 01:17:03 +0200

    Add menu and context menu entries

--- oz_plugin.py
+++ oz_plugin.py
@@ -25,33 +25,79 @@ class OzRunCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         pass
 
+@with_oz
+def feed_region(view):
+    global sp
+    msg = ""
+    for region in view.sel():
+        if not region.empty():
+            msg += view.substr(region)
+    sp.send(msg)
+
 class OzFeedRegion(sublime_plugin.TextCommand):
-    @with_oz
     def run(self, edit):
-        global sp
-        msg = ""
+        feed_region(self.view)
+
+    def is_enabled(self):
         for region in self.view.sel():
             if not region.empty():
-                msg += self.view.substr(region)
-        sp.send(msg)
+                return True
+        return False
+
+    def description(self):
+        "Feed the selected text to the Oz compiler"
+
+
+@with_oz
+def feed_line(view):
+    global sp
+    msg = ""
+    for region in view.sel():
+        if region.empty:
+            line = view.line(region)
+            msg += view.substr(line) + '\n'
+    sp.send(msg)
 
 class OzFeedLine(sublime_plugin.TextCommand):
-    @with_oz
     def run(self, edit):
-        global sp
-        msg = ""
-        for region in self.view.sel():
-            if region.empty:
-                line = self.view.line(region)
-                msg += self.view.substr(line) + '\n'
-        sp.send(msg)
+        feed_line(self.view)
 
-class OzFeedBufferCommand(sublime_plugin.TextCommand):
+    def is_enabled(self):
+        for region in self.view.sel():
+            if region.empty():
+                return True
+        return False
+
+    def description(self):
+        "Feed the current line to the Oz compiler"
+
+class OzFeedBuffer(sublime_plugin.TextCommand):
     @with_oz
     def run(self, edit):
         global sp
         msg = self.view.substr(sublime.Region(0, self.view.size()))
         sp.send(msg)
+
+    def description(self):
+        "Feed the current file to the Oz compiler"
+
+class OzFeedContext(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global sp
+        msg = ""
+        for region in self.view.sel():
+            if not region.empty:
+                return feed_region(self.view)
+        return feed_line(self.view)
+
+    def is_enabled(self):
+        for region in self.view.sel():
+            return True
+        return False
+
+    def description(self):
+        "Feed text to the Oz compiler"
+
 
 def stop():
     global sp
@@ -63,6 +109,10 @@ class OzKillCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         stop()
 
+    def description(self):
+        "Kill the Oz compiler"
+
 class ExitListener(sublime_plugin.EventListener):
     def on_pre_close(view):
         stop()
+
